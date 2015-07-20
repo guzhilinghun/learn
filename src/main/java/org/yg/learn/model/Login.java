921da08ffff1432131c31ac36a46b4bfda73d528
YangGuang <guzhilinghun@qq.com>
2015-07-20 17:34:03 +0800

    11

---
+++ src/main/java/org/yg/learn/model/Login.java
@@ -0,0 +1,22 @@
+package org.yg.learn.model;
+
+import java.util.Date;
+
+public class Login {
+	private Integer id;
+	private Integer userId;
+	private String username;
+	private String password;
+	private String salt;
+	private String state;
+	private Date createdDate;
+	private Date updatedDate;
+	private String createdBy;
+	private String updatedBy;
+	
+	
+	
+	
+	
+
+}
